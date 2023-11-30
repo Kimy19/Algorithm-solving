6da97cd32cf2e01f0768e0e2d320c536454d764a
Kimy19 <yckim19@naver.com>
2023-11-30 18:53:50 +0900

    create 정수삼각형.c

---
+++ dp/정수삼각형.c
@@ -0,0 +1,50 @@
+//백준 정수삼각형 https://www.acmicpc.net/problem/1932
+
+#include <stdio.h>
+
+int n;
+int map[500][500];
+int dp[500][500];
+
+
+int main(void)
+{
+	int index = 0;
+
+	freopen("../input.txt","r",stdin);
+	scanf("%d",&n);
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j<i+1; j++)
+			{
+				scanf("%d ",&map[i][j]);
+			}
+	}
+	//bottom-up
+	dp[0][0] = map[0][0];
+	for(int i = 1; i < n; i++)
+	{
+		for(int j = 0; j<i+1; j++)
+			{
+				if(j == 0)
+				{
+					dp[i][j] = dp[i-1][j] + map[i][j];
+					continue;
+				}
+
+				if(dp[i-1][j-1] < dp[i-1][j])
+					dp[i][j] = dp[i-1][j]+ map[i][j];
+				else
+					dp[i][j] = dp[i-1][j-1]+ map[i][j];
+			}
+	}
+	int max = 0;
+	for(int i = 0; i< n; i++)
+	{
+		if(max < dp[n-1][i])
+			max = dp[n-1][i];
+	}
+	printf("%d",max);
+
+	return (0);
+}
