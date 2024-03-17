e4d52cea9f642cfc8e86937e173a9af1a85af631
Kimy19 <yckim19@naver.com>
2024-03-17 23:06:28 +0900

    [day37] 1로만들기2.py

--- dp/1로만들기2.py
+++ dp/1로만들기2.py
@@ -5,23 +5,14 @@ dp = [0 for _ in range(n+1)]
 route = [0 for _ in range(n+1)]
 
 for i in range(2,n+1):
-    if i % 3 != 0 and i%2 != 0:
-        dp[i] = dp[i-1]+1
-        route[i] = i-1
-    elif i%3==0:
-        if dp[i//3]>= dp[i-1]:
-            dp[i] = dp[i-1]+1
-            route[i] = i-1
-        else:
-            dp[i] = dp[i//3]+1
-            route[i] = i//3
-    elif i%2==0:
-        if dp[i//2]>= dp[i-1]:
-            dp[i] = dp[i-1]+1
-            route[i] = i-1
-        else:
-            dp[i] = dp[i//2]+1
-            route[i] = i//2
+    dp[i] = dp[i - 1] + 1
+    route[i] = i - 1
+    if i % 3 == 0 and dp[i // 3] + 1 < dp[i]:
+        dp[i] = dp[i // 3] + 1
+        route[i] = i // 3
+    if i % 2 == 0 and dp[i // 2] + 1 < dp[i]:
+        dp[i] = dp[i // 2] + 1
+        route[i] = i // 2
 i = n
 ans.append(n)
 while i != 1:
