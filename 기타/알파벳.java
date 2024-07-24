6f29b21499cfbafc1a4218bdb1bbda204bd77a05
Kimy19 <yckim19@naver.com>
2024-07-24 23:05:10 +0900

    [day102] 알파벳.java

---
+++ 기타/알파벳.java
@@ -0,0 +1,18 @@
+//baekjoon 10808
+package src;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int[] count = new int[26];
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i)-'a']++;
+        }
+        for(int i= 0; i<26; i++){
+            System.out.print(count[i]+" ");
+        }
+    }
+}
